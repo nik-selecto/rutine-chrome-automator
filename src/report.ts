75f16fa1e482bf2e9fc068af0996609dc0f9d89e
nik-kita <nik.moiseienko@gmail.com>
2022-02-28 11:37:44 +0200

    feat(video): click more button

--- src/report.ts
+++ src/report.ts
@@ -1,5 +1,11 @@
 import { Page } from 'puppeteer';
+import { VideoSelector } from './selectors/video.selector';
 
 export default async function reportVideo(page: Page, url: string) {
     await page.goto(url);
+    await page.waitForXPath(VideoSelector.MORE_button);
+
+    const [reportButton] = await page.$x(VideoSelector.MORE_button);
+
+    await reportButton.click();
 }
--- src/utils/get-email-password-from-cli.utils.ts
+++ src/utils/get-email-password-from-cli.utils.ts
@@ -1,19 +1,25 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import inquierer from 'inquirer';
 
+// TODO rm hardcode from cli
+
 export default async function getEmailPasswordFromCli() {
-    const { email, password } = await inquierer.prompt([
-        {
-            type: 'input',
-            message: 'Your youtube account email please',
-            name: 'email',
-        },
-        {
-            type: 'password',
-            message: 'Your youtube account password please',
-            name: 'password',
-            mask: true,
-        },
-    ]);
+    // const { email, password } = await inquierer.prompt([
+    //     {
+    //         type: 'input',
+    //         message: 'Your youtube account email please',
+    //         name: 'email',
+    //     },
+    //     {
+    //         type: 'password',
+    //         message: 'Your youtube account password please',
+    //         name: 'password',
+    //         mask: true,
+    //     },
+    // ]);
+
+    const [email, password] = await Promise.resolve(process.argv.slice(2));
 
     if (!email || !/\w+@\w+\.\w+/.test(email)) {
         throw new Error(`First arg should be your youtube's email! Actual value: "${email}"`);
