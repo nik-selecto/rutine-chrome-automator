9d7c78fa0be0a3649efc6e51997fd32a185d74a7
nik-kita <nik.moiseienko@gmail.com>
2022-02-28 14:53:30 +0200

    feat(work): a lot of hardcode but work

--- src/report.ts
+++ src/report.ts
@@ -1,21 +1,52 @@
 import { Page } from 'puppeteer';
-import { VideoSelector } from './selectors/video.selector';
+import { VideoOptionsSelector } from './selectors/video.selector';
+import pause from './utils/pause.utils';
 
 export default async function reportVideo(page: Page, url: string) {
     await page.goto(url);
-    await page.waitForXPath(VideoSelector.MORE_button);
+    await page.waitForXPath(VideoOptionsSelector.MORE_button);
 
-    const [moreButton] = await page.$x(VideoSelector.MORE_button);
+    const [moreButton] = await page.$x(VideoOptionsSelector.MORE_button);
 
     await moreButton.click();
-    await page.waitForXPath(VideoSelector.DESCRIPTION_REPORT_FEEDBACK_buttons);
+    await page.waitForXPath(VideoOptionsSelector.DESCRIPTION_REPORT_FEEDBACK_buttons);
 
-    const [, reportButton] = await page.$x(VideoSelector.DESCRIPTION_REPORT_FEEDBACK_buttons);
+    const [, reportButton] = await page.$x(VideoOptionsSelector.DESCRIPTION_REPORT_FEEDBACK_buttons);
 
     await reportButton.click();
-    await page.waitForXPath(VideoSelector.SPAM_radio);
+    await page.waitForXPath(VideoOptionsSelector.SPAM_radio);
 
-    const [spamRadio] = await page.$x(VideoSelector.SPAM_radio);
+    const [spamRadio] = await page.$x(VideoOptionsSelector.SPAM_radio);
 
     await spamRadio.click();
+    await page.waitForXPath(VideoOptionsSelector.REPORT_checkboxes_5);
+
+    const checkboxes = await page.$x(VideoOptionsSelector.REPORT_checkboxes_5);
+    // TODO rm this hardcode
+    const spamCheckbox = checkboxes[5];
+
+    await spamCheckbox.click();
+    // await pause(5000);
+    await page.waitForXPath(VideoOptionsSelector.SPAM_checkItems_30);
+
+    const checkItems = await page.$x(VideoOptionsSelector.SPAM_checkItems_30);
+    // TODO rm this hardcode
+    const falseInfoCheckItem = checkItems[30];
+
+    // TODO rm this hardcode
+    await pause(1500);
+    await falseInfoCheckItem.click();
+
+    const [nextButton] = await page.$x(VideoOptionsSelector.NEXT_button);
+
+    await nextButton.click();
+    await page.waitForXPath(VideoOptionsSelector.REPORT_textarea);
+
+    const [textarea] = await page.$x(VideoOptionsSelector.REPORT_textarea);
+
+    await textarea.type('This video is not truth. And It\'s goal is to MAKE WAR between Russian and Ukraine!');
+
+    const [, finalReportButton] = await page.$x(VideoOptionsSelector.REPORT_buttons_2);
+
+    await finalReportButton.click();
 }
--- src/selectors/video.selector.ts
+++ src/selectors/video.selector.ts
@@ -1,6 +1,11 @@
 // eslint-disable-next-line no-shadow
-export enum VideoSelector {
+export enum VideoOptionsSelector {
     MORE_button = '//button[starts-with(@aria-label, "Ещё") or starts-with(@aria-label, "More") or starts-with(@aria-label, "Інші")]',
     DESCRIPTION_REPORT_FEEDBACK_buttons = '//tp-yt-paper-item[@role="option"]',
     SPAM_radio = '//*[contains(@aria-label, "Спам") or contains(@aria-label, "Spam")]',
+    REPORT_checkboxes_5 = '//input[@aria-haspopup="listbox" and @role="button"]',
+    SPAM_checkItems_30 = '//tp-yt-paper-item[@role="option"]',
+    NEXT_button = '//yt-formatted-string[contains(@class, "style-blue-text")]',
+    REPORT_textarea = '//textarea',
+    REPORT_buttons_2 = '//yt-formatted-string[contains(@class, "style-blue-text")]',
 }
