b09f04447aecb69ee795de416f2eb2237cde5945
nik-kita <nik.moiseienko@gmail.com>
2022-02-28 12:29:59 +0200

    feat(click): report

--- src/report.ts
+++ src/report.ts
@@ -5,7 +5,12 @@ export default async function reportVideo(page: Page, url: string) {
     await page.goto(url);
     await page.waitForXPath(VideoSelector.MORE_button);
 
-    const [reportButton] = await page.$x(VideoSelector.MORE_button);
+    const [moreButton] = await page.$x(VideoSelector.MORE_button);
+
+    await moreButton.click();
+    await page.waitForXPath(VideoSelector.DESCRIPTION_REPORT_FEEDBACK_buttons);
+
+    const [, reportButton] = await page.$x(VideoSelector.DESCRIPTION_REPORT_FEEDBACK_buttons);
 
     await reportButton.click();
 }
--- src/selectors/video.selector.ts
+++ src/selectors/video.selector.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-shadow
 export enum VideoSelector {
     MORE_button = '//button[starts-with(@aria-label, "Ещё") or starts-with(@aria-label, "More") or starts-with(@aria-label, "Інші")]',
+    DESCRIPTION_REPORT_FEEDBACK_buttons = '//tp-yt-paper-item[@role="option"]',
 }
