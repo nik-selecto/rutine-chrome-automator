66500ceb38f6d6524051745af5403c922d4eae71
nik-selecto <n.moiseienko@selectoglobal.com>
2022-06-29 13:39:18 +0300

    feat: login()

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      // TODO create .env file with your secrets
-      // TODO add their type declarations to this interface
+      EMAIL: string,
+      PASSWORD: string,
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv';
 import puppeteer from 'puppeteer-extra';
 import stealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { generateUrl } from './generate-url';
+import { login } from './login';
 import pause from './utils/pause.utils';
 
 config();
@@ -10,10 +11,12 @@ async function main() {
     try {
         puppeteer.use(stealthPlugin());
 
+        const { EMAIL: email, PASSWORD: password } = process.env;
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
 
         await page.goto(generateUrl());
+        await login(page, { email, password });
         await pause(300000);
 
         await browser.close();
